dae409dccbbf8c9f4914c5053c194e69ae5c9395
Muhammad-Ahmad-Inamdar <mohammad.ainamdar@gmail.com>
2024-03-23 03:37:18 +0500

    Pushed on Github

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const random_number = Math.floor(Math.random() * 6 + 1);
+const awnswer = await inquirer.prompt([
+    { name: 'user_number',
+        type: 'number',
+        message: 'Guess a number in betweem 1 to 6 ',
+    }
+]);
+if (awnswer.user_number === random_number) {
+    console.log(`Conguragulations..!!\nYou Guessed Correctly`);
+}
+else {
+    console.log(`You guessed a wrong number\nThe number was: ${random_number}\nTry Again..!`);
+}
